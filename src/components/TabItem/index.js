40ee4b929a8a9f14a0f412f2596d6aa8c11e4426
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2022-12-23 04:32:48 +0530

    Update index.js

--- src/components/TabItem/index.js
+++ src/components/TabItem/index.js
@@ -1,13 +1,23 @@
 // Write your code here
-import './index.css'
-
 const TabItem = props => {
-  const {tabItem} = props
-  const {displayText} = tabItem
+  const {tabItem, updateTabId, isActive} = props
+  const {tabId, displayText} = tabItem
+
+  const isActiveBtn = isActive ? 'active-tab-btn' : ''
+
+  const onClickButton = () => {
+    updateTabId(tabId)
+  }
 
   return (
     <li>
-      <p>{displayText}</p>
+      <button
+        type="button"
+        className={`tab-btn ${isActiveBtn}`}
+        onClick={onClickButton}
+      >
+        {displayText}
+      </button>
     </li>
   )
 }
